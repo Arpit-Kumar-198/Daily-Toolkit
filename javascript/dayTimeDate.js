117a7776d9c9d312d26f9d04f2d1de6a381cc8a2
Arpit Kumar <arpitkumar19842@gmail.com>
2026-06-06 00:54:10 +0530

    optimize date time update functionality

--- javascript/dayTimeDate.js
+++ javascript/dayTimeDate.js
@@ -1,20 +1,50 @@
- //  Day Time Date section 
-const currentDate = new Date();
-const dateBox = document.querySelector('.date-box');
-// Day
-const day = document.querySelector('.day');
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-day.innerText = days[currentDate.getDay()];
-// Time
-const time = document.querySelector('.time');
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-const period = hours >= 12 ? "PM" : "AM";
-time.innerText = ` ${currentDate.getHours() < 10 ? '0'+currentDate.getHours() : currentDate.getHours()}:${currentDate.getMinutes() < 10 ? '0'+currentDate.getMinutes() : currentDate.getMinutes()} ${period}`;
-// Date
-const date = document.querySelector('.date-box .date');
-const d = currentDate.getDate() < 10 ? `0${currentDate.getDate()}` : currentDate.getDate();
-const m = currentDate.getMonth()+1 < 10 ? `0${currentDate.getMonth() + 1}` : currentDate.getMonth()+1;
-const y = currentDate.getFullYear();
-date.innerText = `${d} / ${m} / ${y}`;
-        
+// Select elements once
+const dayElement = document.querySelector(".day");
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date-box .date");
+
+// Days array
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+// Function to update day, time and date
+function updateDateTime() {
+  const currentDate = new Date();
+
+  // Day
+  dayElement.innerText = days[currentDate.getDay()];
+
+  // Time
+  let hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+
+  const period = hours >= 12 ? "PM" : "AM";
+
+  // If you want 12-hour format
+  hours = hours % 12 || 12;
+
+  const formattedHours = hours < 10 ? `0${hours}` : hours;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  timeElement.innerText = `${formattedHours}:${formattedMinutes} ${period}`;
+
+  // Date
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const year = currentDate.getFullYear();
+
+  dateElement.innerText = `${day} / ${month} / ${year}`;
+}
+
+// Initial call
+updateDateTime();
+
+// Update every second
+setInterval(updateDateTime, 1000);
